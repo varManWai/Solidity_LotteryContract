604851d7ac1247e21194bb7c40f19ac983cbeb5c
varManWai <laizoke98@gmail.com>
2022-11-29 16:33:08 +0800

    added the assert for the reset address for the players

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -93,14 +93,15 @@ describe("Lottery Contract", () => {
     });
 
     const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({ from: accounts[0] });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
 
-    await lottery.methods.pickWinner().send({
+    const players = await lottery.methods.getPlayers().call({
       from: accounts[0],
     });
-
-    const finalBalance = await web3.eth.getBalance(accounts[0]);
-
-    const difference = finalBalance - initialBalance;
+    
     assert(difference > web3.utils.toWei("1.8", "ether"));
+    assert.equal(0, players.length);
   });
 });
