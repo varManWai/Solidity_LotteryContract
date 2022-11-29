f09352a506d5898804e780fcb35dad7e4c41de9e
varManWai <laizoke98@gmail.com>
2022-11-29 18:55:21 +0800

    installed the dotenv package to enable the .env function and changed the account used for contract deployment

--- deploy.js
+++ deploy.js
@@ -11,12 +11,15 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", accounts[0]);
+  console.log("Attempting to deploy from account", accounts[2]);
+
+  // console.log(accounts);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
-    .send({ gas: "1000000", from: accounts[0] });
+    .send({ gas: "1000000", from: accounts[2] });
 
+  console.log(interface);
   console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
